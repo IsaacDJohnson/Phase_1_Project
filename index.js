93393051df6b7bb09301f78ed87d8fec70e79eb8
Isaacjohnson <isaac.designws@gmail.com>
2022-04-12 11:28:07 -0500

    completed

--- index.js
+++ index.js
@@ -15,3 +15,129 @@
 
 //5. Follow good coding practices. Keep your code DRY (Do not repeat yourself) 
 //   by utilizing functions to abstract repetitive code.
+
+function init(){
+    randomDogGet()
+    addEventListeners()
+}
+
+function randomDogGet(){
+    
+    fetch('https://random.dog/woof.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        document.getElementById('randomDogImg').src = `${data.url}`
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+}
+
+function addEventListeners(){
+    let submitForm = document.getElementById('submitForm')
+    submitForm.addEventListener('submit', formPost)
+    submitForm.addEventListener('submit', newRosterForm)
+    document.getElementById('refresh').addEventListener('click', randomDogGet)
+    document.getElementById('dealCardsOne').addEventListener('click', dealCardOne)
+    document.getElementById('dealCardsTwo').addEventListener('click', dealCardTwo)   
+}
+
+function formPost(event){
+    
+    event.preventDefault()
+    
+    const name = document.getElementById('dogName').value
+    const age = document.getElementById('dogAge').value
+    const dogImg = document.getElementById('randomDogImg').src
+    
+    const formData = {
+            name: name,
+            imgUrl: dogImg,
+            age: age
+        }
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+     fetch('http://localhost:3000/dogs', configurationObject)
+     .catch(()=>{
+        alert('Form Post ERROR')
+    }) 
+    event.reset
+}
+
+function newRosterForm(event){
+      
+    const name = document.getElementById('dogName').value
+    const age = document.getElementById('dogAge').value
+    const dogImg = document.getElementById('randomDogImg').src
+    const roster = document.getElementById('playerRoster')   
+    const listItem = document.createElement('li')
+    const node = document.createTextNode('')
+        
+    listItem.appendChild(node)
+    
+    listItem.innerHTML = `<div style="margin: 20px;">
+                          <img src='${dogImg} 'height='200px'' 'width='200px''><br>
+                          <p> name: '${name}'</p>
+                          <p> age: '${age}' </p>
+                          <button id='${name}'>add to game</button>
+                          </div>`
+
+    roster.appendChild(listItem)
+
+    document.getElementById(`${name}`).addEventListener('click', addPlayerToGame)
+    
+    function addPlayerToGame(){
+
+        let playerOneName = document.getElementById('playerOneName')
+        let playerTwoName = document.getElementById('playerTwoName')
+        let playerOne = document.getElementById('playerOneImg')
+        let playerTwo = document.getElementById('playerTwoImg')
+
+        if (playerOneName.innerText === ''){
+            playerOne.src = dogImg
+            playerOneName.innerText = `${name}`
+        } else {
+            playerTwo.src = dogImg
+            playerTwoName.innerText = `${name}`
+        }
+
+    }
+    
+}
+
+function dealCardOne(){
+    let hand = document.getElementById('cardHandOne')
+    let total = hand.value
+    let randomNum = Math.random() * (10 - 1) + 1
+    let newTotal = total +++ Math.round(randomNum)
+    
+    if (total <= 20){
+        hand.value = newTotal     
+    } else if (total >= 21){
+        alert('Player one loses')
+    }
+}
+function dealCardTwo(){
+    let hand = document.getElementById('cardHandTwo')
+    let total = hand.value
+    let randomNum = Math.random() * (10 - 1) + 1
+    let newTotal = total +++ Math.round(randomNum)
+    
+    if (total <= 20){
+        hand.value = newTotal
+    } else if (total >= 20){
+        alert('Player Two loses')
+    }
+}
+
+document.addEventListener('DOMContentLoaded', init)
