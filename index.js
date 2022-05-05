d4bef802ce287afac3d0ec62b55d4f14836f64f2
Isaacjohnson <isaac.designws@gmail.com>
2022-05-05 13:13:21 -0500

    added more functionality and placed comments throughout for ease of understanding

--- index.js
+++ index.js
@@ -22,8 +22,9 @@ function init(){
 }
 
 function randomDogGet(){
-    
+    //random dog image API with image sources under key name "message"
     fetch('https://dog.ceo/api/breeds/image/random')
+    //await promise response
     .then((response) => {
         return response.json();
     })
@@ -36,6 +37,7 @@ function randomDogGet(){
 }
 
 function addEventListeners(){
+    //adds desired event listeners for on page load
     let submitForm = document.getElementById('submitForm')
     submitForm.addEventListener('submit', formPost)
     submitForm.addEventListener('submit', newRosterForm)
@@ -47,7 +49,7 @@ resetForm = () => {
 }
 
 function formPost(event){
-    
+    //no refresh
     event.preventDefault()
     
     const name = document.getElementById('dogName').value
@@ -68,26 +70,26 @@ function formPost(event){
         },
         body: JSON.stringify(formData)
       };
-
-     fetch('http://localhost:3000/dogs', configurationObject)
-     .catch(()=>{
+    //fetch POST to local db.json with dog card form data
+    fetch('http://localhost:3000/dogs', configurationObject)
+    .catch(()=>{
         alert('Form Post ERROR')
     }) 
     randomDogGet()
 }
 
-function newRosterForm(event){
-    
+function newRosterForm(){
+    //input field values
     const dogName = document.getElementById('dogName').value
     const age = document.getElementById('dogAge').value
     const dogImg = document.getElementById('randomDogImg').src
-    const roster = document.getElementById('rosterList')   
-    const listItem = document.createElement('li')
+   
+    //variable declerations for new list items
+    const roster = document.getElementById('rosterList');
+    const listItem = document.createElement('li');
     listItem.className = 'new-list-item'
-    // const node = document.createTextNode('')
-        
-    // listItem.appendChild(node)
-    
+   
+    //HTML to be added to roster
     listItem.innerHTML = `<div class='card' style='float: left; margin-left:40px; align:top;'>
                           <img src='${dogImg} 'height='200px'' 'width='200px''><br>
                           <p> name: '${dogName}'</p>
@@ -97,14 +99,28 @@ function newRosterForm(event){
 
     roster.appendChild(listItem)
 
-    document.getElementById(`${dogName}`).addEventListener('click', createPlayer)
+    document.getElementById(`${dogName}`).addEventListener('click', checkPlayerCount)
     
+    //checks list for number of players and dictates the addition of new players.
+    function checkPlayerCount(){
+    //length of ul
+        if (document.querySelectorAll('ul#gameTable li').length > 1){
+            alert("You're trying to add to many players.")
+        } else {
+            //if true call:
+            createPlayer()
+        }
+    }
+
+
     function createPlayer(){
         
-        document.getElementById('dealer').addEventListener('click', dealCards)
+        //empty ul
         const gameRoom = document.getElementById('gameTable')
+        
+        //dynamic creation of li's
         const newListItem = document.createElement('li')
-        newListItem.className = 'new-list-item'
+        newListItem.className = 'new-list-item'      
         const newNode = document.createTextNode('')
         newListItem.appendChild(newNode)
         
@@ -116,24 +132,27 @@ function newRosterForm(event){
         age: <p>${age} </p>
         <input type="text" class="card-total" id= ${dogName} value="0">
         </div>`
-
+        //added to ul
         gameRoom.appendChild(newListItem)
-    
+        
+        document.getElementById('dealer').addEventListener('click', dealCards)
+        
         function dealCards(){
+            
+            //objects to store values using querySelectorAll
             let [dogNameOne, dogNameTwo] = document.querySelectorAll('.dog-name')
-            let [dogTotalOne, dogTotalTwo] = document.querySelectorAll('.card-total')
-            // let handOne = document.getElementById(`${dogName}`)
-            // let handtwo = document.getElementById(`${dogName}`)
-
+            let [dogTotalOne, dogTotalTwo] = document.querySelectorAll('.card-total');
             let strTotalOne = parseInt(dogTotalOne.value)
             let strTotalTwo = parseInt(dogTotalTwo.value)
-
+            
+            //math to generate numerals and add to existing card totals
             let randomNumOne = Math.random() * (10 - 1) + 1
             let randomNumTwo = Math.random() * (10 - 1) + 1
             dogTotalOne.value = Math.round(randomNumOne) + strTotalOne
             dogTotalTwo.value = Math.round(randomNumTwo) + strTotalTwo
 
-            // debugger
+            //debugger
+            //if to check for card values
             if (parseInt(dogTotalOne.value) === 21 || parseInt(dogTotalTwo.value) === 21){
                 parseInt(dogTotalOne.value) === 21 ? alert(`21! ${dogNameOne.textContent} wins!`) : alert(`21! ${dogNameTwo.textContent} wins!`)
     
@@ -146,6 +165,8 @@ function newRosterForm(event){
         }
 
     }
+    //empties submitForm
+    resetForm()
 }
 document.addEventListener('DOMContentLoaded', init)
 
